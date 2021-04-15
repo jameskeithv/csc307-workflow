d1450e74a4a836ba41add544169385abd9722ecb
Caroline <cpadil12@calpoly.edu>
2021-04-14 21:59:37 -0700

    Updated file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,8 @@
-System.out.println("Keith Vega, 4th year");
+class HelloWorld{ 
+   public static void main(String[] args){
+      System.out.println("Keith Vega, 4th year");
+      System.out.println("Hi, bestie"); 
+   } 
+}
 
 
